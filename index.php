e5e28f244d6c852dc866487215580095e25b9457
Synnly <emanuelfernandes54@gmail.com>
2023-12-05 21:59:18 +0100

    Ajout de la connection d'un utilisateur au site

--- index.php
+++ index.php
@@ -9,16 +9,38 @@
 </head>
 <body>
     <header>
-        <form method="get">
-            <legend>Login</legend>
-            <input type="text">
-            <legend>Mot de passe</legend>
-            <input type="password">
-            <input type="submit" value="Connexion" name="submitConnect">
-        </form>
-        <form action="creerCompte.php">
-            <button type="submit">Créer un compte</button>
-        </form>
+        <?php
+
+            include "configBD.php";
+            if($_GET['submitConnect'] == "Connexion"){  // Demande de connexion envoyée
+
+                $mysqli=mysqli_connect($_IPBD, $_USERNAME, $_PASSWORD, $_NAMEBD);
+                $sql="SELECT * FROM Utilisateurs WHERE login='".$_GET['login']."' AND password='".$_GET['password']."';";
+                $resultat=mysqli_query($mysqli,$sql) or die("$sql : ".mysqli_error($mysqli));
+
+                setcookie("login", $_GET['login'], time()+3600);
+
+                if($resultat->num_rows == 1){    // Connexion réussie
+                    echo "<p>Bienvenue</p>";
+                }
+                else{
+                    echo "<p>Erreur de connexion</p>";
+                }
+            }
+            else{
+                echo "<form method=\"get\">
+                        <legend>Login</legend>
+                        <input type=\"text\" name=\"login\">
+                        <legend>Mot de passe</legend>
+                        <input type=\"password\" name=\"password\">
+                        <input type=\"submit\" value=\"Connexion\" name=\"submitConnect\">
+                    </form>
+                    <form action=\"creerCompte.php\">
+                        <button type=\"submit\">Créer un compte</button>
+                    </form>";
+            }
+
+        ?>
     </header>
 </body>
 </html>
