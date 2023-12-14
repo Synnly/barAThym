3a6c3e13206c4863ce652fd8d040c916fcadee69
Synnly <emanuelfernandes54@gmail.com>
2023-12-14 16:07:06 +0100

    Ajout de résistance aux injections sql, hashage du mot de passe dans la bd

--- creerCompte.php
+++ creerCompte.php
@@ -12,7 +12,7 @@
         include "configBD.php";
 
         function afficherPageLogin(){
-            echo "<div id=\"signin\"><form method=\"get\">
+            echo "<div id=\"divFormulaire\"><div id=\"signin\"><form method=\"post\">
                 <legend>Login</legend>
                 <input type=\"text\" name=\"login\" required=\"required\">
         
@@ -52,47 +52,66 @@
                 <br>
                 <input type=\"submit\" value=\"Valider\" name=\"submitCreate\">
             </form></div>
-            <p>ou <a href=\"index.php\">se connecter</a></p> 
+            <p>ou <a href=\"index.php\">se connecter</a></p></div>
             ";
         }
 
         
-        if(!isset($_GET['submitCreate'])){  // Demande de création de compte non envoyée
+        if(!isset($_POST['submitCreate'])){  // Demande de création de compte non envoyée
             afficherPageLogin();
         }
         else{
-            if($_GET['submitCreate'] == "Valider" && $_GET['login'] != "" && $_GET['password'] != ""){
-                
-                // Verification de l'unicité du login
-                $mysqli= mysqli_connect($_IPBD, $_USERNAME, $_PASSWORD, $_NAMEBD);
-                $res = $mysqli->query("SELECT * FROM Utilisateurs WHERE login='".$_GET['login']."';");
-                
-                // On verifie si le login est déjà utilisé
-                if($res->num_rows > 0){
-                    echo "Ce login est déjà utilisé";
-                    unset($_GET['submitCreate']);
-                    $mysqli->close();
+            if($_POST['submitCreate'] == "Valider" && $_POST['login'] != "" && $_POST['password'] != ""){
+
+                if(preg_match("/[\*\.\'\/\"\(\)\{\}\[\]\$\;\:\&\|]+/", $_POST['login'])){    // Login contient des caractères interdits
+                    echo "<h3>Le login ne peut pas contenir ces caractères : *.'/\"(){}[]$;:&|</h3>";
                     afficherPageLogin();
                 }
-                else{
-                    // Ajout de l'utilisateur dans la base de données
-                    $login = $_GET['login']; 
-                    $password = $_GET['password'];
-                    $nom = $_GET['nom'] == '' ? "NULL" : "'".$_GET['nom']."'";
-                    $prenom = $_GET['prenom'] == '' ? "NULL" : "'".$_GET['prenom']."'";
-                    $sexe = $_GET['sexe'] == '' ? "NULL" : "'".$_GET['sexe']."'";
-                    $email = $_GET['email'] == '' ? "NULL" : "'".$_GET['email']."'";
-                    $dateNaissance = $_GET['dateNaissance'] == '' ? "NULL" : "'".$_GET['dateNaissance']."'";
-                    $adresse = $_GET['adresse'] == '' ? "NULL" : "'".$_GET['adresse']."'";
-                    $codePostal = $_GET['codePostal'] == '' ? "NULL" : "'".$_GET['codePostal']."'";
-                    $ville = $_GET['ville'] == '' ? "NULL" : "'".$_GET['ville']."'";
-                    $telephone = $_GET['telephone'] == '' ? "NULL" : "'".$_GET['telephone']."'";
+    
+                elseif(preg_match("/[\*\.\'\/\"\(\)\{\}\[\]\$\;\:\&\|]+/", $_POST['password'])){     // Mot de passe contient des caractères interdits
+                    echo "Le mot de passe ne peut pas contenir ces caractères : *.'/\"(){}[]$;:&|";
+                    afficherPageLogin();
+                }
+
+                else{   // Login et mot de passe valides
                     
-                    $sql = "INSERT INTO Utilisateurs VALUES ('$login','$password',$nom,$prenom,$sexe,$email,$dateNaissance,$adresse,$codePostal,$ville,$telephone);";
-                    $mysqli = new mysqli($_IPBD, $_USERNAME, $_PASSWORD, $_NAMEBD);
-                    $mysqli->query($sql);
-                    header("Location: index.php");  // Redirection vers la page principale
-                    unset($_GET['submitCreate']);
+                    try{
+                        $pdo = new PDO('mysql:host='.$_IPBD.';dbname='.$_NAMEBD, $_USERNAME, $_PASSWORD);
+                    }
+                    catch(Exception $e){
+                        exit($e->getMessage());
+                    }
+                   
+                    // Requete du mot de passe
+                    $sql="SELECT * FROM Utilisateurs WHERE login = ? ;";
+                    $res = $pdo->prepare($sql);
+                    $res->execute([$_POST['login']]);
+
+                    // On verifie si le login est déjà utilisé
+                    if($res->rowCount() > 0){
+                        echo "Ce login est déjà utilisé";
+                        afficherPageLogin();
+                    }
+                    else{
+                        // Ajout de l'utilisateur dans la base de données
+                        $login = $_POST['login'];
+                        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                        $nom = $_POST['nom'] == '' ? "NULL" : "'".$_POST['nom']."'";
+                        $prenom = $_POST['prenom'] == '' ? "NULL" : "'".$_POST['prenom']."'";
+                        $sexe = $_POST['sexe'] == '' ? "NULL" : "'".$_POST['sexe']."'";
+                        $email = $_POST['email'] == '' ? "NULL" : "'".$_POST['email']."'";
+                        $dateNaissance = $_POST['dateNaissance'] == '' ? "NULL" : "'".$_POST['dateNaissance']."'";
+                        $adresse = $_POST['adresse'] == '' ? "NULL" : "'".$_POST['adresse']."'";
+                        $codePostal = $_POST['codePostal'] == '' ? "NULL" : "'".$_POST['codePostal']."'";
+                        $ville = $_POST['ville'] == '' ? "NULL" : "'".$_POST['ville']."'";
+                        $telephone = $_POST['telephone'] == '' ? "NULL" : "'".$_POST['telephone']."'";
+
+                        $sql = "INSERT INTO Utilisateurs VALUES ('$login','$password',$nom,$prenom,$sexe,$email,$dateNaissance,$adresse,$codePostal,$ville,$telephone);";
+                        $mysqli = new mysqli($_IPBD, $_USERNAME, $_PASSWORD, $_NAMEBD);
+                        $mysqli->query($sql);
+                        header("Location: index.php");  // Redirection vers la page principale
+                    }
+                    unset($_POST['submitCreate']);
                 }
             }
             else{
